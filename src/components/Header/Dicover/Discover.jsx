e1e10c7caa99927b69537b40ba7a21ce668c5708
Angel Landkoer <angel.landkoer09@gmail.com>
2022-07-21 14:07:12 -0500

    creando componente discover y llamandolo desde header y dándole su contexto

---
+++ src/components/Header/Dicover/Discover.jsx
@@ -0,0 +1,17 @@
+import "./Discover.scss";
+
+export function Discover() {
+  return (
+    <>
+      <section className="Discover">
+        <h1>Discover a New Era of Crypto Currency</h1>
+        <p>
+          Panda NFT is the primier marketplace for NFT, which are digital items
+          you can truly own. Digital items have existed for a long time, but
+          never like this.
+        </p>
+        <button>Get Started</button>
+      </section>
+    </>
+  );
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 // import styles
 import "./Header.scss";
 import { NavBar } from "./NavBar/NavBar";
+import { Discover } from "./Dicover/Discover"
 
 export function Header() {
   return (
     <header>
       <section className="header">
         <NavBar />
+        <Discover />
       </section>
     </header>
   );
