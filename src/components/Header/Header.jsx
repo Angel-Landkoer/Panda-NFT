a92f01ebf03ec808a835832e1297309538e49b3d
Angel Landkoer <angel.landkoer09@gmail.com>
2022-07-28 18:55:46 -0500

    creando un mock para traerme datos a la card del header, agregando una promesa paraallamar el mock creado y mmostrarlo econ unmap en la card los datos necesarios

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,48 @@
+// import hooks
+import { useState, useEffect } from "react";
+
 // import styles
 import "./Header.scss";
+
+// import components
 import { NavBar } from "./NavBar/NavBar";
 import { Discover } from "./Dicover/Discover";
 import { CardNFT } from "./CardNFT/CardNFT";
 
+// import utilities
+import { data } from "../../utilities/Card.mock";
+
 export function Header() {
+  const [cardData, setCardData] = useState([]);
+
+  const getPromose = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(data);
+    }, 2000);
+  });
+
+  useEffect(() => {
+    getPromose
+      .then((res) => {
+        return setCardData(res);
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+
+
   return (
     <header>
       <section className="header">
         <NavBar />
         <Discover />
-        <CardNFT
-          description={"Heat Wave No GIF by Loof and Timmy"}
-          image={
-            "https://media3.giphy.com/media/TeRP089ltwj4Ib61aj/giphy.gif?cid=d13c8921t1eayoo1h5fbxjoroy9mttl8qt7e0cbvu7sqiff5&rid=giphy.gif&ct=g"
-          }
-        />
+        {
+          cardData
+          .map((item, i) => {
+            return (
+              <CardNFT key={i} description={item.title} image={item.images.original.url} />
+            )
+          })
+        }
       </section>
     </header>
   );
---
+++ src/utilities/Card.mock.js
@@ -0,0 +1,123 @@
+export const data = [
+  // {
+  //   type: 'gif',
+  //   id: 'hAxskB68DrJwv5ZhNA',
+  //   url: 'https://giphy.com/gifs/ufc-hAxskB68DrJwv5ZhNA',
+  //   slug: ' ufc-hAxskB68DrJwv5ZhNA',
+  //   bitly_gif_url: 'https://gph.is/g/4oAWNm5',
+  //   bitly_url: 'https://gph.is/g/4oAWNm5',
+  //   embed_url: 'https://giphy.com/embed/hAxskB68DrJwv5ZhNA',
+  //   username: 'ufc',
+  //   source: 'http://ufc.com',
+  //   title: 'Claudia Gadelha Idk GIF by UFC',
+  //   rating: 'g',
+  //   images: {
+  //     original: {
+  //       height: 320,
+  //       width: 480,
+  //       size: 1081822,
+  //       url: 'https://media1.giphy.com/media/hAxskB68DrJwv5ZhNA/giphy.gif?cid=d13c8921slef3fbt0cy53tzikamcnvc5wflg7ir4j0wvl11j&rid=giphy.gif&ct=g',
+  //     }
+  //   },
+  //   user: {
+  //     avatar_url: 'https://media0.giphy.com/channel_assets/ufc/ejy3O7Yzea89.png',
+  //     banner_image: 'https://media0.giphy.com/headers/ufc/lbvixwADb5xs.jpg',
+  //     banner_url: 'https://media0.giphy.com/headers/ufc/lbvixwADb5xs.jpg',
+  //     profile_url: 'https://giphy.com/ufc/',
+  //     username: 'ufc',
+  //     display_name: 'UFC',
+  //     description: 'All of the UFC GIFs!',
+  //   }
+
+  // },
+  // {
+  //   type: "gif",
+  //   id: "MCeIiRETfwBK2rtGRi",
+  //   url: "https://giphy.com/gifs/birthday-happy-happybirthday-MCeIiRETfwBK2rtGRi",
+  //   slug: "birthday-happy-happybirthday-MCeIiRETfwBK2rtGRi",
+  //   bitly_gif_url: "https://gph.is/g/aNBdyKv",
+  //   bitly_url: "https://gph.is/g/aNBdyKv",
+  //   embed_url: "https://giphy.com/embed/MCeIiRETfwBK2rtGRi",
+  //   username: "travisfosterstudio",
+  //   source: "https://www.travisfoster.com/",
+  //   title: "Happy Birthday Smile GIF by Travis Foster",
+  //   rating: "g",
+  //   images: {
+  //     original: {
+  //       height: "480",
+  //       width: "480",
+  //       size: "403706",
+  //       url: "https://media0.giphy.com/media/MCeIiRETfwBK2rtGRi/giphy.gif?"
+  //     }
+  //   },
+  //   user: {
+  //     avatar_url: "https://media3.giphy.com/avatars/travisfosterstudio/9zrDQA8En47j.gif",
+  //     banner_image: "",
+  //     banner_url: "",
+  //     profile_url: "https://giphy.com/travisfosterstudio/",
+  //     username: "travisfosterstudio",
+  //     display_name: "Travis Foster",
+  //     description: "Illustrator + animator,\r\ncontent creator\r\nWant to work together?",
+  //   }
+  // },
+  // {
+  //   type: "gif",
+  //   id: "qjGEWzXWcwbMgX7cNt",
+  //   url: "https://giphy.com/gifs/sharkweek-sharks-islandofthewalkingsharks-epaulettesharks-qjGEWzXWcwbMgX7cNt",
+  //   slug: "sharkweek-sharks-islandofthewalkingsharks-epaulettesharks-qjGEWzXWcwbMgX7cNt",
+  //   bitly_gif_url: "https://gph.is/g/4gKKQ77",
+  //   bitly_url: "https://gph.is/g/4gKKQ77",
+  //   embed_url: "https://giphy.com/embed/qjGEWzXWcwbMgX7cNt",
+  //   username: "sharkweek",
+  //   source: "",
+  //   title: "Walk Sharks GIF by Shark Week",
+  //   rating: "g",
+  //   images: {
+  //     original: {
+  //       height: "270",
+  //       width: "480",
+  //       size: "9193852",
+  //       url: "https://media2.giphy.com/media/qjGEWzXWcwbMgX7cNt/giphy.gif?cid=d13c8921slef3fbt0cy53tzikamcnvc5wflg7ir4j0wvl11j&rid=giphy.gif&ct=g",
+  //     }
+  //   },
+  //   user: {
+  //     avatar_url: "https://media2.giphy.com/avatars/sharkweek/cCKyaPudrtjx.png",
+  //     banner_image: "https://media2.giphy.com/channel_assets/sharkweek/zUqM00Q5nvVk.png",
+  //     banner_url: "https://media2.giphy.com/channel_assets/sharkweek/zUqM00Q5nvVk.png",
+  //     profile_url: "https://giphy.com/sharkweek/",
+  //     username: "sharkweek",
+  //     display_name: "Shark Week",
+  //     description: "Live every week like it's well... us!",
+  //   }
+  // },
+  {
+    type: "gif",
+    id: "tsX3YMWYzDPjAARfeg",
+    url: "https://giphy.com/gifs/tiktok-bear-tsX3YMWYzDPjAARfeg",
+    slug: "tiktok-bear-tsX3YMWYzDPjAARfeg",
+    bitly_gif_url: "https://gph.is/g/4D3zkPZ",
+    bitly_url: "https://gph.is/g/4D3zkPZ",
+    embed_url: "https://giphy.com/embed/tsX3YMWYzDPjAARfeg",
+    username: "tiktok",
+    source: "bit.ly/2BcAl0y",
+    title: "Happy So Excited GIF by TikTok",
+    rating: "g",
+    images: {
+      original: {
+        height: "420",
+        width: "329",
+        size: "3547769",
+        url: "https://media2.giphy.com/media/tsX3YMWYzDPjAARfeg/giphy.gif?cid=d13c8921slef3fbt0cy53tzikamcnvc5wflg7ir4j0wvl11j&rid=giphy.gif&ct=g",
+      }
+    },
+    user: {
+      avatar_url: "https://media1.giphy.com/avatars/tiktok/78F6dPcUBc73.gif",
+      banner_image: "https://media1.giphy.com/headers/tiktok/SRLJgKV9HbQK.gif",
+      banner_url: "https://media1.giphy.com/headers/tiktok/SRLJgKV9HbQK.gif",
+      profile_url: "https://giphy.com/tiktok/",
+      username: "tiktok",
+      display_name: "TikTok",
+      description: "Get the app now  ↓   ↓  ↓\r\nhttp://vm.tiktok.com/mxhMwf/",
+    }
+  }
+];
